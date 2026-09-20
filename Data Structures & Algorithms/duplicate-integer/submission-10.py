081f4954688619c1567c759b8d27ab914d0b9120
Dharmik Madupathi <dharmik.madupathi@gmail.com>
2026-09-20 17:33:26 -0400

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        myMap = {} 
+
+        for i in range(len(nums)):
+            if nums[i] in myMap: 
+                return True
+            else: 
+                myMap[nums[i]] = 1
+        return False
